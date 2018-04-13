dde5034aed6ad8ba2e0dc1c9042b01795f8f7942
knarjp <kevajess@gmail.com>
2018-04-13 18:11:57 -0400

    initial commit branch

--- src/Model/AreaEffect/OneShotAreaEffect.java
+++ src/Model/AreaEffect/OneShotAreaEffect.java
@@ -1,4 +1,4 @@
 package Model.AreaEffect;
 
-public class OneShotAreaEffect {
+public class OneShotAreaEffect { // TODO: kevin implement (I made this comment so my branch on my desktop can be accessed by my laptop and I'm lazy
 }
