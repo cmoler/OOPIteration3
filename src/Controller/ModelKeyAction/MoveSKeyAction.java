fc3ee1741539dc2efbb603bfe8ed962221991ba6
Colton <coltonmoler@gmail.com>
2018-04-15 15:10:12 -0400

    added in changing orientation

--- src/Controller/ModelKeyAction/MoveSKeyAction.java
+++ src/Controller/ModelKeyAction/MoveSKeyAction.java
@@ -1,6 +1,7 @@
 package Controller.ModelKeyAction;
 
 import Model.Entity.Entity;
+import Model.Entity.EntityAttributes.Orientation;
 import com.sun.javafx.geom.Vec3d;
 import javafx.scene.input.KeyCode;
 
@@ -16,6 +17,7 @@ public class MoveSKeyAction extends ModelKeyAction {
     @Override
     public void handle(KeyCode incomingKey) {
         if(incomingKey == keyCode){
+            entity.setOrientation(Orientation.SOUTH);
             entity.addVelocity(new Vec3d(0, -1, 1));
         }
     }
--- src/Controller/ModelKeyAction/MoveSWKeyAction.java
+++ src/Controller/ModelKeyAction/MoveSWKeyAction.java
@@ -1,6 +1,7 @@
 package Controller.ModelKeyAction;
 
 import Model.Entity.Entity;
+import Model.Entity.EntityAttributes.Orientation;
 import com.sun.javafx.geom.Vec3d;
 import javafx.scene.input.KeyCode;
 
@@ -16,6 +17,7 @@ public class MoveSWKeyAction extends ModelKeyAction {
     @Override
     public void handle(KeyCode incomingKey) {
         if(incomingKey == keyCode){
+            entity.setOrientation(Orientation.SOUTHWEST);
             entity.addVelocity(new Vec3d(-1, 0 , 1));
         }
     }
