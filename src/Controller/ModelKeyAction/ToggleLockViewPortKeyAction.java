9a116e95ede52b3d616a39eb2819cdff875e59f3
Colton <coltonmoler@gmail.com>
2018-04-15 16:54:25 -0400

    started working on toggling the viewport

--- src/Controller/ModelKeyAction/ToggleLockViewPortKeyAction.java
+++ src/Controller/ModelKeyAction/ToggleLockViewPortKeyAction.java
@@ -1,21 +1,31 @@
 package Controller.ModelKeyAction;
 
+import Controller.Factories.ControllerSetFactory;
 import Model.Entity.Entity;
 import javafx.scene.input.KeyCode;
 
 public class ToggleLockViewPortKeyAction extends ModelKeyAction {
 
     private Entity entity;
+    private ControllerSetFactory controllerSetFactory;
+    private boolean controllingPlayer;
 
-    public ToggleLockViewPortKeyAction(KeyCode keyCode, Entity entity) {
+    public ToggleLockViewPortKeyAction(KeyCode keyCode, Entity entity, ControllerSetFactory controllerSetFactory, boolean controllingPlayer) {
         super(keyCode);
         this.entity = entity;
+        this.controllerSetFactory = controllerSetFactory;
+        this.controllingPlayer = controllingPlayer;
     }
 
     @Override
     public void handle(KeyCode incomingKey) {
         if(incomingKey == keyCode){
-
+            if(controllingPlayer){
+                controllerSetFactory.createScrollingViewPortSet();
+            }
+            else{
+                controllerSetFactory.createPlayerControlsSet(entity);
+            }
         }
     }
 
