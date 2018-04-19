945ee5c686378081fcd838f6f8b78aabf70653f3
Colton <coltonmoler@gmail.com>
2018-04-19 15:46:44 -0400

    moving resets scroll

--- src/Controller/ModelKeyAction/MoveNKeyAction.java
+++ src/Controller/ModelKeyAction/MoveNKeyAction.java
@@ -1,17 +1,21 @@
 package Controller.ModelKeyAction;
 
+import Controller.GameLoop;
 import Model.Entity.Entity;
 import Model.Entity.EntityAttributes.Orientation;
 import com.sun.javafx.geom.Vec3d;
+import javafx.geometry.Point2D;
 import javafx.scene.input.KeyCode;
 
 public class MoveNKeyAction extends ModelKeyAction {
 
     private Entity entity;
+    private GameLoop gameLoop;
 
-    public MoveNKeyAction(KeyCode keyCode, Entity entity) {
+    public MoveNKeyAction(KeyCode keyCode, Entity entity, GameLoop gameLoop) {
         super(keyCode);
         this.entity = entity;
+        this.gameLoop = gameLoop;
     }
 
     @Override
@@ -19,6 +23,7 @@ public class MoveNKeyAction extends ModelKeyAction {
         if(incomingKey == keyCode){
             entity.setOrientation(Orientation.NORTH);
             entity.addVelocity(new Vec3d(0, 1, -1));
+            gameLoop.setScrollOffSet(Point2D.ZERO);
         }
     }
 
