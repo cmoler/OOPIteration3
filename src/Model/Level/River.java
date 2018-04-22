081d5efe44f7d04e8db78de9d7f25dcc4d964a5d
Colton <coltonmoler@gmail.com>
2018-04-22 18:53:16 -0400

    rivers push every .5 seconds

--- src/Model/Level/River.java
+++ src/Model/Level/River.java
@@ -7,13 +7,27 @@ import com.sun.javafx.geom.Vec3d;
 
 public class River implements Visitable {
     private Vec3d flowrate;
+    private long nextMoveTime = 0;
+    private long speed = 0500000000l;
 
     public River(Vec3d flowrate) {
         this.flowrate = flowrate;
     }
 
+    public Vec3d getPush(){
+        if(canMove()) {
+            setNextMoveTime();
+            return flowrate;
+        }
+        return new Vec3d(0, 0,0 );
+    }
+
     public Vec3d getFlowrate() {
         return flowrate;
+    }
+
+    public void setSpeed(long speed){
+        this.speed = speed;
     }
 
     public void setFlowrate(Vec3d flowrate) {
@@ -26,5 +40,13 @@ public class River implements Visitable {
 
     public boolean isTraversable(Entity e) {
         return e.getTraversalStrength() > flowrate.length();
+    }
+
+    private boolean canMove(){
+        return System.nanoTime() > nextMoveTime;
+    }
+
+    private void setNextMoveTime() {
+        nextMoveTime = System.nanoTime() + speed;
     }
 }
